fe01db6a42eaf1fc133472656a1b7e43687230cc
bartvanhoey <bartvanhoey@hotmail.com>
2020-04-17 20:36:19 +0200

    end-part-6

--- BlazorPokemon.Models/Pokemon.cs
+++ BlazorPokemon.Models/Pokemon.cs
@@ -4,6 +4,29 @@ namespace BlazorPokemon.Models
 {
     public class Pokemon
     {
+        private string _photoPath;
+        public Pokemon()
+        {
+
+        }
+
+        public Pokemon(int id, string name, string type1, string type2, int total, int hP, int attack, int defense, int speedAttack, int speedDefense, int speed, int generation, bool legendary)
+        {
+            Id = id;
+            Name = name;
+            Type1 = type1;
+            Type2 = type2;
+            Total = total;
+            HP = hP;
+            Attack = attack;
+            Defense = defense;
+            SpeedAttack = speedAttack;
+            SpeedDefense = speedDefense;
+            Speed = speed;
+            Generation = generation;
+            Legendary = legendary;
+        }
+
         public int PokemondId { get; set; }
         public int Id { get; set; }
         public string Name { get; set; }
@@ -18,5 +41,19 @@ namespace BlazorPokemon.Models
         public int Speed { get; set; }
         public int Generation { get; set; }
         public bool Legendary { get; set; }
+        public string PhotoPath
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_photoPath))
+                {
+                    return "images/" + Name.ToLowerInvariant() + ".png";
+                }
+                return _photoPath;
+
+            }
+
+            set { _photoPath = value; }
+        }
     }
 }
