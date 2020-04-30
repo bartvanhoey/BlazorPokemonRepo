265aecf42117539cb15256eedb77e954d9411ce7
bartvanhoey <bartvanhoey@hotmail.com>
2020-04-30 16:38:33 +0200

    end-part-25

---
+++ BlazorPokemon.Web/Pages/DataBindingDemoBase.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorPokemon.Web.Pages
+{
+    public class DataBindingDemoBase : ComponentBase
+    {
+        protected string Name { get; set; } = "Tom";
+        
+        protected string Gender { get; set; } = "Male";
+
+        public string Colour { get; set; } = "background-color:gray";
+
+    }
+}
