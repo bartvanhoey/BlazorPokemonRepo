48802537b52f7de235e55fec988aeae2b619f71f
bartvanhoey <bartvanhoey@hotmail.com>
2020-05-11 16:25:10 +0200

    end-part-39

---
+++ BlazorPokemon.Web/Models/EditPokemonProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using BlazorPokemon.Models;
+
+namespace BlazorPokemon.Web.Models
+{
+    public class EditPokemonProfile : Profile
+    {
+        public EditPokemonProfile(){
+                CreateMap<Pokemon, EditPokemonModel>()
+                    .ForMember(dest => dest.ConfirmEmail, opt => opt.MapFrom(src => src.Email));
+                CreateMap<EditPokemonModel, PokemonType>();
+        }
+    }
+}
