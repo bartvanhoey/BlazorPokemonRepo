62bb1fa3c4504f87068d2e89718e1cb1e7f850ab
bartvanhoey <bartvanhoey@hotmail.com>
2020-05-11 15:24:59 +0200

    end-part-37

---
+++ BlazorPokemon.Web/Models/EditPokemonModel.cs
@@ -0,0 +1,37 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using BlazorPokemon.Models;
+using BlazorPokemon.Models.CustomValidators;
+
+namespace BlazorPokemon.Web.Models
+{
+    public class EditPokemonModel
+    {
+        public int PokemonId { get; set; }
+        public int PokemonNumber { get; set; }
+        [Required(ErrorMessage = "You must provide a Name")]
+        [MinLength(2)]
+        public string Name { get; set; }
+        [EmailAddress]
+        [EmailDomainValidator(AllowedDomain = "PragimTech.com", ErrorMessage = "Only PragimTech.com is an allowed Domain")]
+        public string Email { get; set; }
+        [CompareProperty("Email", ErrorMessage = "Email and Confirm Email must match")]
+        public string ConfirmEmail { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Gender Gender { get; set; }
+        public int? TypeOneId { get; set; }
+        public int? TypeTwoId { get; set; }
+        public int Total { get; set; }
+        public int HP { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int SpeedAttack { get; set; }
+        public int SpeedDefense { get; set; }
+        public int Speed { get; set; }
+        public int Generation { get; set; }
+        public bool Legendary { get; set; }
+        public string PhotoPath { get; set; }
+        public PokemonType TypeOne { get; set; }
+        public PokemonType TypeTwo { get; set; }
+    }
+}
